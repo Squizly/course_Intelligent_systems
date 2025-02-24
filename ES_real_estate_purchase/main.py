24262ea1076fc89899afd788b6d3703c7503ef1f
Petr Kolesov <petrkolesov010204@gmail.com>
2025-02-24 21:07:18 +0700

    🟩 Объявлен класс экспертной системы + некоторые методы
    # Ввод ответов на вопросы в self.facts
    # Вывод ответов в терминал
    # Проверка бюджета (предусмотрен числовой ввод, который потом интерпретируется в строковый, на основе анализа недвижимости на Авито)

--- ES_real_estate_purchase/main.py
+++ ES_real_estate_purchase/main.py
@@ -1,12 +1,61 @@
 from knowledge_base.questions import questions
 
+class RealEstateExpertSystem():
+
+    def __init__(self, questions):
+        self.questions = questions
+    
+    def ask_question(self):
+        # Получаем ответы на вопросы от пользователя в словарь facts
+        self.facts = dict()
+
+        for ask in self.questions:
+            self.facts[ask["Ключ"]] = input(ask["Вопрос"])
+    
+    def print_answers_user(self):
+        # Вывод ответов пользователя
+        for key,value in self.facts.items():
+            print(f"{key} : {value}")
+    
+    def predict(self):
+        # Учитывая ответы пользователя , строим цепочку выводов
+
+        if self.check_correct_budget() is False:
+            print("Рекомендую увеличить бюджет.")
+            return
+        
+        # Сделаем скоринг для каждого варианта, чтоб вывести топ вариантов
+
+        
+
+    def check_correct_budget(self):
+
+        if (int(self.facts["Бюджет"]) < 1_000_000):
+
+            if (self.facts["Ипотека"] == "Нет"):
+                print("Ваш бюджет слишком мал для покупки недвижимости.")
+                return False
+            
+            self.facts["Бюджет"] = "Низкий"
+
+        elif (int(self.facts["Бюджет"]) < 3_000_000):
+            self.facts["Бюджет"] = "Низкий"
+
+        elif (int(self.facts["Бюджет"]) < 5_000_000):
+            self.facts["Бюджет"] = "Средний"
+
+        else:
+            self.facts["Бюджет"] = "Высокий"
+        
+        return True
+        
+        
+
+
 
 if __name__ == "__main__":
 
-    answers = {}
-
-    for q in questions:
-        answers[q["Ключ"]] = input(q["Значение"])
-    
-    for key, value in answers.items():
-        print(f"{key} : {value}")
+    system = RealEstateExpertSystem(questions)
+    system.ask_question()
+    system.print_answers_user()
+    system.predict()
