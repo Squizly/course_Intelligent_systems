d26cc82175616ecc3557b5a11dc870152df0be4a
Petr Kolesov <petrkolesov010204@gmail.com>
2025-03-07 06:52:06 +0700

    🟨 Подключили библиотеки и запустили приложение

--- ES_real_estate_purchase/main.py
+++ ES_real_estate_purchase/main.py
@@ -3,18 +3,9 @@ import re
 from knowledge_base.questions import *
 from knowledge_base.rules import *
 from expert_system import *
+from gui import *
 
+if __name__ == "__main__":
 
-expert_system = RealEstateExpert(questions, rules)
-
-expert_system.get_facts()
-
-# Прямой вывод результата со списком рекомендаций --->
-expert_system.fit()
-
-
-# Обратный вывод результата --->
-for rule in rules:
-
-    if (expert_system.backward_chaining(rule["Результат"])):
-        print(f"Система рекомендует : {rule["Результат"]}")
+    app = RealEstateApp()
+    app.mainloop()
