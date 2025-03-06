ebf18c64b538e708fc2c31f599c390818ab38846
Petr Kolesov <petrkolesov010204@gmail.com>
2025-03-06 09:36:24 +0700

    🟨 Рефакторинг, проверка цепочечного вывода

--- ES_real_estate_purchase/main.py
+++ ES_real_estate_purchase/main.py
@@ -1,8 +1,19 @@
+import re
+
 from knowledge_base.questions import *
 from knowledge_base.rules import *
 from expert_system import *
 
-if __name__ == "__main__":
-    system = RealEstateExpertSystem(questions, rules)
-    system.get_facts()
-    system.predict()
+
+expert_system = RealEstateExpert(questions, rules)
+
+expert_system.get_facts()
+expert_system.convert_budget_to_category_type()
+
+facts = set(expert_system.facts.values())
+print(facts)
+
+
+
+if (expert_system.backward_chaining("Однокомнатная студия эконом-класса в спальном районе", facts)):
+    print(" ---> достигнут")
